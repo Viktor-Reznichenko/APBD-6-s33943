72573cffc4453c3ec6bd57e238cc4098141b6cdf
s33943 <s33943@pjwstk.edu.pl>
2026-04-14 11:45:16 +0200

    Controllers first commit

---
+++ APBD_6/APBD_6/Controllers/RoomsController.cs
@@ -0,0 +1,44 @@
+﻿using APBD_6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_6.Controllers;
+
+public class RoomsController : ControllerBase
+{
+    public static List<Room> rooms = new List<Room>()
+    {
+       
+    };
+    //GET api/rooms
+    [HttpGet]
+    public IActionResult Get([FromQuery] int? id = 0)
+    {
+        // 200 ok
+        return Ok(rooms);
+    }
+    
+    [Route("{id}")]
+    [HttpGet]
+    public IActionResult GetById(int id)
+    {
+        var room = rooms.FirstOrDefault(x => x.Id == id);
+        if (room == null)
+        {
+            return NotFound();
+        }
+        return Ok(room);
+    }
+    
+    [Route("building/{buildingCode}")]
+    [HttpGet]
+    public IActionResult GetByBuilding(string buildingCode)
+    {
+        var buildingRooms = rooms.Where(x => x.BuildingCode.Equals(buildingCode)).ToList();
+        if (buildingRooms == null)
+        {
+            return NotFound();
+        }
+        return Ok(buildingRooms);
+    }
+    
+}
