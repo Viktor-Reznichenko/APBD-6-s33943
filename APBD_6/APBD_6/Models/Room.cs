9e4c6734786048c1873d8cd99ffb06a647013516
s33943 <s33943@pjwstk.edu.pl>
2026-04-14 11:20:13 +0200

    Room model commit

---
+++ APBD_6/APBD_6/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_6.Models;
+
+public class Room
+{
+    public int Id{get;set;}
+    public string Name{get;set;}
+    public char BuildingCode{get;set;}
+    public int Floor{get;set;}
+    public int Capacity{get;set;}
+    public bool HasProjector { get; set; }
+    public bool IsActive { get; set; }
+}
