bcc0e0fc8463a2319fc3f90ec48ddf3ca66d96c0
s33943 <s33943@pjwstk.edu.pl>
2026-04-14 11:23:44 +0200

    Reservation model commit

---
+++ APBD_6/APBD_6/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_6.Models;
+
+public class Reservation
+{
+    public int Id{get;set;}
+    public int RoomId{get;set;}
+    public string OrganizerName{get;set;}
+    public string Topic{get;set;}
+    public DateTime Date{get;set;}
+    public DateTime StartTime{get;set;}
+    public DateTime EndTime{get;set;}
+    public string Status{get;set;}
+}
