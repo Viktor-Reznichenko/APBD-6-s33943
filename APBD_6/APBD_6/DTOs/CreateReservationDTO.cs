ed1b80bc3f530b0be53c0436d7f6e00b87c005f2
s33943 <s33943@pjwstk.edu.pl>
2026-04-21 00:29:38 +0300

    CreateReservationDTO is created

---
+++ APBD_6/APBD_6/DTOs/CreateReservationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD_6.DTOs;
+
+public class CreateReservationDTO
+{
+    public int RoomId{get;set;}
+    public string OrganizerName{get;set;}
+    public string Topic{get;set;}
+    public DateTime Date{get;set;}
+    public DateTime StartTime{get;set;}
+    public DateTime EndTime{get;set;}
+    public string Status{get;set;}
+}
